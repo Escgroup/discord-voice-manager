6a8d7098ab647c078e3ff0a7d68b8dd04385f7e7
mouse_484 <escmouse484@gmail.com>
2020-02-22 00:25:22 +0000

    fix #8

--- src/check.js
+++ src/check.js
@@ -1,33 +1,34 @@
 const fs = require("fs");
 
 module.exports = (client, config) => {
-    let db = JSON.parse(fs.readFileSync(`${__dirname}/db.json`));
+  let db = JSON.parse(fs.readFileSync(`${__dirname}/db.json`));
 
-    db.forEach((element, index) => {
-        const time = Number(element.time);
-        if (Date.now() - time > 300000) {
-            const voice_channel = client.channels.get(element.channel_id);
+  db.forEach((element, index) => {
+    const time = Number(element.time);
+    if (Date.now() - time > 300000) {
+      const voice_channel = client.channels.get(element.channel_id);
 
-            try {
-                if (voice_channel.members.first()) {
-                    db[index].time = Date.now();
-                    fs.writeFileSync(
-                        `${__dirname}/db.json`,
-                        JSON.stringify(db)
-                    );
-                } else {
-                    voice_channel.delete();
-                    client.channels
-                        .get(config.log)
-                        .messages.get(element.invite_msg)
-                        .delete();
-                    db.splice(index, 1);
-                    fs.writeFileSync(
-                        `${__dirname}/db.json`,
-                        JSON.stringify(db)
-                    );
-                }
-            } catch {}
+      try {
+        if (voice_channel.members.first()) {
+          db[index].time = Date.now();
+          fs.writeFileSync(
+            `${__dirname}/db.json`,
+            JSON.stringify(db)
+          );
+        } else {
+          voice_channel.delete();
+          client.channels
+            .get(config.log)
+            .messages.get(element.invite_msg)
+            .delete();
+          db.splice(index, 1);
+          fs.writeFileSync(
+            `${__dirname}/db.json`,
+            JSON.stringify(db)
+          );
+          console.log(`create channel: ${voice_channel.name}`)
         }
-    });
+      } catch { }
+    }
+  });
 };
