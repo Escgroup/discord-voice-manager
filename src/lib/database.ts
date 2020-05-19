827f80f25899b07fb0e3cd29c25921a6a8608deb
mouse_484 <escmouse484@gmail.com>
2020-05-19 05:49:14 +0000

    add db

---
+++ src/lib/database.ts
@@ -0,0 +1,30 @@
+import { Snowflake } from 'discord.js';
+import { promises as fs } from 'fs';
+
+type dataType = {
+  channel_id: Snowflake;
+  channel_name: string;
+  user_id: Snowflake;
+  user_name: string;
+  time: number;
+  invite_msg: Snowflake;
+};
+
+class database {
+  public path = `${__dirname}/db.json`;
+  constructor() {}
+  async add(item: dataType) {
+    const file = await this.getFile();
+    file.push(item);
+    this.save(file);
+  }
+  private async getFile() {
+    const file: [dataType] = JSON.parse(await fs.readFile(this.path, 'utf-8'));
+    return file;
+  }
+  private save(item: [dataType]) {
+    fs.writeFile(this.path, JSON.stringify(item));
+  }
+}
+
+export const db = new database();
