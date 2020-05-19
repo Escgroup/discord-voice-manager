9a17990e72f2ef539e9d368aa87a678f9e473eb0
mouse_484 <escmouse484@gmail.com>
2020-05-19 01:48:05 +0000

    ready not setup

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,4 @@
 import { Event, Client } from 'ecstar';
-import { setup } from '../lib/setup';
 
 export = class extends Event {
   constructor(client: Client) {
@@ -7,7 +6,7 @@ export = class extends Event {
   }
   run() {
     this.client.user?.setActivity('通話管理Bot');
-    setup(this.client);
+    
     // setInterval(() => check(client, config), 60000);
   }
 };
