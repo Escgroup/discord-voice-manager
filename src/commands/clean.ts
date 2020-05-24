b098c36a672d0b333525db56d5a0ea2239b0031c
mouse_484 <escmouse484@gmail.com>
2020-05-24 12:50:28 +0000

    Owner only clean

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -3,7 +3,7 @@ import { Message } from 'discord.js';
 
 export = class extends Command {
   constructor(client: Client) {
-    super(client, { name: 'clean' });
+    super(client, { name: 'clean', ownerOnly: true });
   }
   run(message: Message) {
     message.channel.bulkDelete(99);
